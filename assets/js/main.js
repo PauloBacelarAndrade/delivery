935b0f49eb1992ecfc6269e8e3f34a37c4397772
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-10-11 10:56:48 -0300

    Wrote linkAction and showMenu functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SHOW SCROLL UP ===============*/
-
 
 /*=============== DARK LIGHT THEME ===============*/
